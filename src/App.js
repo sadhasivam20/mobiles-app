b81ee89d4a1da82e2f28f359d25ede174ba02eee
sadhasivam20 <sadha2040@gmail.com>
2023-01-08 11:51:13 +0530

    API from node

--- src/App.js
+++ src/App.js
@@ -1,57 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
-    <div className="App"> 
-<PhoneList />
+    <div className="App">
+      <PhoneList />
     </div>
   );
 }
-function PhoneList(){
-  const mobiles= [{
-    "model": "OnePlus 9 5G",
-    "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-    "company": "Oneplus"
-    },
-    {
-    "model": "Iphone 13 mini",
-    "img": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-    "company": "Apple"
-    },
-    {
-    "model": "Samsung s21 ultra",
-    "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-    "company": "Samsung"
-    },
-    {
-    "model": "Xiomi mi 11",
-    "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-    "company": "Xiomi"
-    }
-    ]
-    return(
-      <div>
-         {mobiles.map((mc)=>( 
-<Phone mobiles={mc}/>
+
+function PhoneList() {
+
+  const [mobiles, setMobiles] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/mobiles")
+      .then((data) => data.json())
+      .then((mbs) => setMobiles(mbs));
+  }, []);
+
+  return (
+    <div className="phone-list-container">
+      {mobiles.map((mc) => (
+        <Phone mobile={mc} />
       ))}
-      </div>
-    )
+      console.log(mobile);
+    </div>
+  );
 }
-function Phone({mobiles}){
-  // singlee value can be created..
-  // const mobiles={
-  //   "model": "OnePlus 9 5G",
-  //   "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-  //   "company": "Oneplus"
-  //   }
-    return(
-      <div className='phone-container'>
-        <img className='phone-picture'   src={mobiles.img}></img>
-        <p className='phone-name'>{mobiles.model}</p>
-        <p className='phone-company'>{mobiles.company}</p>
-      </div>
-    )
+function Phone({ mobile }) {
+  return (
+    <div className="phone-container">
+      <img src={mobile.img} className="phone-picture" />
+      <h2 className="phone-name">{mobile.model}</h2>
+      <p className="phone-company">{mobile.company}</p>
+    </div>
+  );
 }
 
 export default App;
